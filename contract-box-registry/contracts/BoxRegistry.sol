57a4435b648937ed19b406516fda6b4c14eb78f6
Alex Zhao <alexzhao.mailbox@gmail.com>
2024-04-28 23:15:29 -0700

    above

--- contract-box-registry/contracts/BoxRegistry.sol
+++ contract-box-registry/contracts/BoxRegistry.sol
@@ -7,15 +7,13 @@ pragma solidity ^0.8.24;
 contract Box {
     address payable internal immutable owner;
     BoxRegistry registry;
-    uint256 registryIndex;
 
     event Withdrawal(uint amount);
     event Deposit(uint amount);
 
-    constructor(BoxRegistry _registry, uint256 _registryIndex, address payable _owner) payable {
+    constructor(BoxRegistry _registry, address payable _owner) payable {
         owner = _owner;
         registry = _registry;
-        registryIndex = _registryIndex;
     }
 
     modifier onlyOwner {
@@ -26,13 +24,9 @@ contract Box {
     function withdraw() public onlyOwner {
         uint256 amount = address(this).balance;
         owner.transfer(address(this).balance);
-        registry.removeBox(owner,registryIndex);
+        registry.removeBox(owner);
         emit Withdrawal(amount);
     }
-
-    // function getBalance() public view returns (uint) {
-    //     return address(this).balance;
-    // }
 
     fallback() external payable {emit Deposit(msg.value);}
 
@@ -49,7 +43,7 @@ contract BoxRegistry {
 
     // Function to create a new Box contract and add it to the user's list of boxes
     function createBox() public {
-        Box newBox = new Box(this,userBoxes[msg.sender].length,payable(msg.sender));
+        Box newBox = new Box(this,payable(msg.sender));
         userBoxes[msg.sender].push(newBox);
         emit BoxCreation(newBox);
     }
@@ -60,8 +54,12 @@ contract BoxRegistry {
         return userBoxes[msg.sender];
     }
 
-    function removeBox(address owner, uint256 index) public {
-        require(msg.sender == address(userBoxes[owner][index]));
+    function removeBox(address owner) public {
+        uint256 index=0;
+        while (index<userBoxes[owner].length && address(userBoxes[owner][index])!=msg.sender) {
+            index++;
+        }
+        require(index<userBoxes[owner].length, "Box not found");
         for (uint256 i=index; i<userBoxes[owner].length-1; i++) {
             userBoxes[owner][i] = userBoxes[owner][i+1];
         }
